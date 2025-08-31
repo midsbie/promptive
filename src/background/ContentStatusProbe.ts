94c1de439281519dd25a5ce64cffb0ee6eac78b5
Miguel Guedes <miguel@softgeist.com>
2025-08-31 11:16:41 +0100

    background: fix malfunctioning content status probe

--- src/background/ContentStatusProbe.ts
+++ src/background/ContentStatusProbe.ts
@@ -1,9 +1,9 @@
-import { MSG, createMessage, send } from "../lib/messaging";
+import { MSG, createMessage, sendToTab } from "../lib/messaging";
 
 export class ContentStatusProbe {
-  async isActive(_tabId: number): Promise<boolean> {
+  async isActive(tabId: number): Promise<boolean> {
     try {
-      const res = await send(createMessage(MSG.QUERY_STATUS));
+      const res = await sendToTab(tabId, createMessage(MSG.QUERY_STATUS));
       return !!res?.active;
     } catch {
       // No receiver / restricted page / timeout → treat as not active
